3602c07efb6f6cc145388b1a6e9f78f8f23ce9fd
suyeon100 <xxyx9811@gmail.com>
2026-05-18 12:51:51 +0900

    feat: Campaign Creat Read Develop

--- src/main/java/com/back/domain/campaign/repository/CampaignRepository.java
+++ src/main/java/com/back/domain/campaign/repository/CampaignRepository.java
@@ -1,4 +1,10 @@
 package com.back.domain.campaign.repository;
 
-public interface CampaignRepository {
+import com.back.domain.campaign.entity.Campaign;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * 캠페인 엔티티에 대한 데이터 접근을 담당하는 저장소입니다.
+ */
+public interface CampaignRepository extends JpaRepository<Campaign, Long> {
 }
